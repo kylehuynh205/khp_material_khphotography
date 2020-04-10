97052e79d233dc0421f6e7038719a4d2160f93c5
Khai Huynh <huynhk@yorku.ca>
2020-04-10 18:16:28 -0400

    fixed

--- js/script_khphotography.js
+++ js/script_khphotography.js
@@ -7,15 +7,24 @@
             }).justifiedGallery({
                 "rowHeight": 150,
             });
+            
+            $('#album-photostream').lightGallery({
+                thumbnail: true,
+                selector: 'a'
+            }).justifiedGallery({
+                "rowHeight": 150,
+            });
+
+            $('#justifiied-photostream').lightGallery({
+                thumbnail: true,
+                selector: 'a'
+            }).justifiedGallery({
+                "rowHeight": 150,
+            });
         });
 
 
-        $('#justifiied-photostream').lightGallery({
-            thumbnail: true,
-            selector: 'a'
-        }).justifiedGallery({
-            "rowHeight": 150,
-        });
+
 
     });
 }(window, jQuery));
